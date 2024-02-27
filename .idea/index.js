858baef22d2a641a9e247391162a53715e479a38
kevolsen <113117373+kevolsen@users.noreply.github.com>
2024-02-27 13:52:55 +0100

    Lagt til regex på tlf/epost. Feltene blir nå også tømt etter salg av billett.

--- .idea/index.js
+++ .idea/index.js
@@ -3,10 +3,19 @@ function Bestill() {
     let antall=document.getElementById("antall").value;
     let fornavn=document.getElementById("fornavn").value;
     let etternavn=document.getElementById("etternavn").value;
-    let tlfnr=document.getElementById("tlfnr").value;
-    let epost=document.getElementById("epost").value;
+    var tlfnr=document.getElementById("tlfnr").value;
+    var epost=document.getElementById("epost").value;
     let biletter=document.getElementById("alleBiletter");
     let enBilett="";
+
+    var tlfREGX = /^[0-9]{8}$/;
+    var epostREGX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    var tlfnrResultat =
+        tlfREGX.test(tlfnr);
+
+    var epostResultat =
+        epostREGX.test(epost);
 
     // Resetter errormeldinger dersom skjema blir sendt inn på nytt
     document.getElementById("errorAntall").innerHTML="";
@@ -15,41 +24,64 @@ function Bestill() {
     document.getElementById("errorTlf").innerHTML="";
     document.getElementById("errorEpost").innerHTML="";
 
+    let error=0;
+
     // Errormeldinger
     if (filmer==0) {
         document.getElementById("errorFilmer").innerHTML="Vennligst velg film";
+        ++error;
     } else {
         enBilett+=""+filmer;
     }
     if (antall==0) {
         document.getElementById("errorAntall").innerHTML="Vennligst fyll inn antall";
+        ++error;
     } else {
         enBilett+=" "+antall;
     }
     if(fornavn==0){
         document.getElementById("errorFornavn").innerHTML="Vennligst fyll inn fornavn";
+        ++error;
     } else {
         enBilett+=" "+fornavn;
     }
     if(etternavn==0){
         document.getElementById("errorEtternavn").innerHTML="Vennligst fyll inn etternavn";
+        ++error;
     } else {
         enBilett+=" "+etternavn;
     }
     if(tlfnr==0){
         document.getElementById("errorTlf").innerHTML="Vennligst fyll inn telefon nr.";
-    } else {
-        enBilett+=" "+tlfnr;
+        ++error;
+    } else if(tlfnrResultat==0) {
+        document.getElementById("errorTlf").innerHTML = tlfnr + " er ikke et gyldig telefonnummer.";
+        ++error;
     }
+        else {
+            enBilett+=" "+tlfnr;
+        }
     if(epost==0){
         document.getElementById("errorEpost").innerHTML="Vennligst fyll inn e-postadresse";
-    } else {
+        ++error;
+    }  else if(epostResultat==0){
+        document.getElementById("errorEpost").innerHTML = epost + " er ikke en gyldig e-postadresse.";
+        ++error;
+    }
+    else {
         enBilett+=" "+epost+"<br>";
     }
 
-    if (filmer!==0 && antall!==0 && fornavn!==0 && etternavn!==0 && tlfnr!==0 && epost!==0) {
-        biletter.innerHTML+=enBilett;
-    }
+        if(error==0){
+            biletter.innerHTML+=enBilett;
+            document.getElementById('filmer').value='';
+            document.getElementById('epost').value='';
+            document.getElementById('tlfnr').value='';
+            document.getElementById('etternavn').value='';
+            document.getElementById('fornavn').value='';
+            document.getElementById('antall').value='';
+        }
+
 }
 function Slett() {
     document.getElementById("alleBiletter").innerHTML="";
